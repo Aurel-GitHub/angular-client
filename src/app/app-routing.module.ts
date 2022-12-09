86c3d71ea874c5ebce7c9a8bc3fa8bfea94a3b63
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-12-09 18:20:59 +0100

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/authentication/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./private/private.module').then(m => m.PrivateModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'authentication',
+    loadChildren: () =>
+      import('./core/authentication/authentication.module').then(
+        m => m.AuthenticationModule
+      ),
   },
   { path: '**', redirectTo: '' },
 ];
